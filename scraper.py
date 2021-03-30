99acefd978cafeccbe944e73af8e391686fbf2d1
Alexia McDonald <alexiasmcdonald@gmail.com>
2021-03-30 23:43:09 +0200

    Add command line args and extra info

--- scraper.py
+++ scraper.py
@@ -1,9 +1,11 @@
 import requests
 from pprint import pprint
 from bs4 import BeautifulSoup
-import re
+from yrke import Yrke
+import sys
 
-URL = 'https://sok.riksarkivet.se/person?Namn=peter+tottie'
+
+URL = 'https://sok.riksarkivet.se/person?Namn=' + sys.argv[1]
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -20,18 +22,24 @@ results = results.replace('<b>', '') # remove <b> html tag
 results = results.replace('</b>', '') # remove </b> html tag
 results = results.replace('<br/>', '') # remove <br/> html tag
 
-
 revised_soup = BeautifulSoup(results, 'html.parser')
 
+# finds name and href within a tags and puts them into lists
 names = []
+hrefs = []
 for name in revised_soup.find_all('a'):
     names.append(name.text)
+    hrefs.append('https://sok.riksarkivet.se' + name.get('href'))
 
+# finds a tags and puts into a list
 a_tags = list(revised_soup.find_all('a'))
 
+# replaces all a tags with link with actual name of person in the results
 index = 0
 for tag in a_tags:
-    results = results.replace(str(a_tags[index]), str(names[index]), 1)
+    url_data = Yrke.get_yrke_page(hrefs[index])
+    add_in = str(names[index]) + '\n' + url_data
+    results = results.replace(str(a_tags[index]), add_in, 1)
     index += 1
 
 print("NUMBER OF RESULTS", result_count)
